cab508ad8aa49531d92729b004f954f312136927
DundIR-PC <ruzin-de@yandex.ru>
2026-07-22 18:00:09 +0500

    Обновление версии

--- jira-issue-copier.user.js
+++ jira-issue-copier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Кнопки копирования задач Jira
 // @namespace    http://tampermonkey.net/
-// @version      1.2.3
+// @version      1.3
 // @description  Копирует номер и название задачи из детальной панели доски Jira
 // @author       DundIIR
 // @match        https://jira.silaunion.ru/*
